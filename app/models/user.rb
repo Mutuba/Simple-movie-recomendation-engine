9e9b44961a27a512a9cc97b894a4a4efccc42264
Mutuba <danielmutubait@gmail.com>
2024-07-05 12:22:09 +0300

    Play with foreign key reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   include Recommendation
 
   validates :name, presence: true
-  has_many : articles, dependent: :destroy
+  has_many :articles, dependent: :destroy
   has_many :liked_movies, dependent: :destroy
   has_many :movies, through: :liked_movies
   set_association :movies
