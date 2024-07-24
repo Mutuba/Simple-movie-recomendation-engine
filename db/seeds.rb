682e6b2ee25e18a1636388116525c996982ecd86
Mutuba <danielmutubait@gmail.com>
2024-07-24 12:53:55 +0300

    Add more SQL queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -245,3 +245,104 @@ end
 # JOIN EmployeeCount ec on employee.id = ec.manager_id 
 # JOIN EmployeeCount ec2 on employee.manager_id = ec2.manager_id
 # where ec.employee_count =  ec2.employee_count
+
+
+# Trips:
+# +------+-----------+-----------+---------+---------------------+------------+
+# | id   | client_id | driver_id | city_id | status              | request_at |
+# +------+-----------+-----------+---------+---------------------+------------+
+# |    1 |         1 |        10 |       1 | completed           | 2023-07-12 |
+# |    2 |         2 |        11 |       1 | cancelled_by_driver | 2023-07-12 |
+# |    3 |         3 |        12 |       6 | completed           | 2023-07-12 |
+# |    4 |         4 |        13 |       6 | cancelled_by_client | 2023-07-12 |
+# |    5 |         1 |        10 |       1 | completed           | 2023-07-13 |
+# |    6 |         2 |        11 |       6 | completed           | 2023-07-13 |
+# |    7 |         3 |        12 |       6 | completed           | 2023-07-13 |
+# |    8 |         2 |        12 |      12 | completed           | 2023-07-14 |
+# |    9 |         3 |        10 |      12 | completed           | 2023-07-14 |
+# |   10 |         4 |        13 |      12 | cancelled_by_driver | 2023-07-14 |
+# +------+-----------+-----------+---------+---------------------+------------+
+
+# Users:
+# +----------+--------+--------+
+# | users_id | banned | role   |
+# +----------+--------+--------+
+# |        1 | No     | client |
+# |        2 | Yes    | client |
+# |        3 | No     | client |
+# |        4 | No     | client |
+# |       10 | No     | driver |
+# |       11 | No     | driver |
+# |       12 | No     | driver |
+# |       13 | No     | driver |
+# +----------+--------+--------+
+
+# CREATE TABLE Trips (
+#   id INT,
+#   client_id INT,
+#   driver_id INT,
+#   city_id INT,
+#   status VARCHAR(20),
+#   request_at DATE
+# );
+
+# INSERT INTO Trips (id, client_id, driver_id, city_id, status, request_at) VALUES
+#   (1, 1, 10, 1, 'completed', '2023-07-12'),
+#   (2, 2, 11, 1, 'cancelled_by_driver', '2023-07-12'),
+#   (3, 3, 12, 6, 'completed', '2023-07-12'),
+#   (4, 4, 13, 6, 'cancelled_by_client', '2023-07-12'),
+#   (5, 1, 10, 1, 'completed', '2023-07-13'),
+#   (6, 2, 11, 6, 'completed', '2023-07-13'),
+#   (7, 3, 12, 6, 'completed', '2023-07-13'),
+#   (8, 2, 12, 12, 'completed', '2023-07-14'),
+#   (9, 3, 10, 12, 'completed', '2023-07-14'),
+#   (10, 4, 13, 12, 'cancelled_by_driver', '2023-07-14');
+
+# CREATE TABLE Users (
+#   users_id INT,
+#   banned VARCHAR(3),
+#   role VARCHAR(10)
+# );
+
+# INSERT INTO Users (users_id, banned, role) VALUES
+#   (1, 'No', 'client'),
+#   (2, 'Yes', 'client'),
+#   (3, 'No', 'client'),
+#   (4, 'No', 'client'),
+#   (10, 'No', 'driver'),
+#   (11, 'No', 'driver'),
+#   (12, 'No', 'driver'),
+#   (13, 'No', 'driver');
+
+
+#   WITH UnbannedTrips AS (
+#     SELECT
+#         t.request_at AS Day,
+#         COUNT(CASE WHEN t.status LIKE 'cancelled%' THEN 1 END) AS canceled_requests,
+#         COUNT(*) AS total_requests
+#     FROM
+#         Trips t
+#         JOIN Users u1 ON t.client_id = u1.users_id
+#         JOIN Users u2 ON t.driver_id = u2.users_id
+#     WHERE
+#         u1.banned = 'No' AND u2.banned = 'No'
+#     GROUP BY
+#         t.request_at
+# )
+# SELECT
+#     Day,
+#     ROUND(canceled_requests / NULLIF(total_requests, 0), 2) AS `Cancellation Rate`
+# FROM
+#     UnbannedTrips
+# ORDER BY
+#     Day;
+
+
+#     +------------+-------------------+
+#     | Day        | Cancellation Rate |
+#     +------------+-------------------+
+#     | 2023-07-12 |              0.33 |
+#     | 2023-07-13 |              0.00 |
+#     | 2023-07-14 |              0.50 |
+#     +------------+-------------------+
+#     3 rows in set (0.01 sec
